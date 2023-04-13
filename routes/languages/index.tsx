6515d8e1cecbc46d838093415181ef010d62fc89
Valentino Melis <tinoanime.my@gmail.com>
2023-04-14 01:25:08 +0200

    Portfolio start

---
+++ routes/languages/index.tsx
@@ -0,0 +1,16 @@
+import { Head } from "$fresh/runtime.ts";
+
+export default function Languages() {
+  return (
+    <>
+      <Head>
+        <title>Languages</title>
+      </Head>
+      <div class="p-4 mx-auto max-w-screen-md">
+        <p class="my-6">
+          All the languages i work with
+        </p>
+      </div>
+    </>
+  );
+}
---
+++ routes/languages/rust/index.tsx
@@ -0,0 +1,16 @@
+import { Head } from "$fresh/runtime.ts";
+
+export default function Rust() {
+  return (
+    <>
+      <Head>
+        <title>Rust Lang</title>
+      </Head>
+      <div class="p-4 mx-auto max-w-screen-md">
+        <p class="my-6">
+          Rust is my favorite language
+        </p>
+      </div>
+    </>
+  );
+}
